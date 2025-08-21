101bed5e9eefe628c2ff613406fe90146afdf4c8
Sarfaraz <sarfarazdyp@gmail.com>
2025-08-21 14:53:21 +0530

    add-on screen UI improvement

--- lib/screens/addOn/activity_add_on.dart
+++ lib/screens/addOn/activity_add_on.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/material.dart';
+
 class ActivityAddOn extends StatelessWidget {
   const ActivityAddOn({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Add-On Screen"),),
+    );
   }
 }
